6dc1ac695ec867a1d88da6dbc265cc3895560e98
solerb09 <46010243+solerb09@users.noreply.github.com>
2024-03-05 21:49:49 -0500

    final draft

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,19 @@ from app import app
 @app.route('/')
 def hello():
     return '<h1>Form test</h1>'
+
+@app.route('/add/<num1>/<num2>')
+def add(num1, num2):
+    return f'<h1> {num1} + {num2} = {int(num1) + int(num2)}</h1>'
+
+@app.route('/subtract/<num1>/<num2>')
+def subract(num1, num2):
+    return f'<h1> {num1} - {num2} = {int(num1) - int(num2)}</h1>'
+
+@app.route('/multiply/<num1>/<num2>')
+def multiply(num1, num2):
+    return f'<h1> {num1} * {num2} = {int(num1) * int(num2)}</h1>'
+
+@app.route('/division/<num1>/<num2>')
+def division(num1, num2):
+    return f'<h1> {num1} / {num2} = {int(num1) // int(num2)}</h1>'
